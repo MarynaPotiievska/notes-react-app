b76a48d2b250c313b0987a2d5faad35cc7e82c48
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-08-08 12:13:41 +0300

    added start notes list

---
+++ src/data/notesList.js
@@ -0,0 +1,51 @@
+export const notesList = [
+  {
+    id: "1",
+    name: "Lily's birthday",
+    created: new Date("07/25/2023"),
+    category: "Task",
+    content: "To prepare a present for Lily until 8/8/2023",
+  },
+  {
+    id: "2",
+    name: "Hair color",
+    created: new Date("07/27/2023"),
+    category: "Random Thought",
+    content: "It would be interesting to look at myself with a red hair",
+  },
+  {
+    id: "3",
+    name: "A gift for Lily",
+    created: new Date("07/28/2023"),
+    category: "Idea",
+    content: "It's may be an umbrella or Harry Potter's theme gift-box",
+  },
+  {
+    id: "4",
+    name: "Shopping List",
+    created: new Date("07/29/2023"),
+    category: "Task",
+    content: "1) Milk; 2) Bread; 3) Lemon; 4) Tomatoes",
+  },
+  {
+    id: "5",
+    name: "TypeScript",
+    created: new Date("07/31/2023"),
+    category: "Idea",
+    content: "It will be good to learn TypeScript",
+  },
+  {
+    id: "6",
+    name: "Questions for an interview",
+    created: new Date("08/01/2023"),
+    category: "Task",
+    content: "Prepare to the interview",
+  },
+  {
+    id: "7",
+    name: "Game Of Thrones",
+    created: new Date("08/02/2023"),
+    category: "Random Thought",
+    content: "Are there some news about George R.R. Martin 6th book?",
+  },
+];
